4bb3ee271d247e88d2db09ea616b4cf471394261
phr3nzy <adilosama47@gmail.com>
2020-03-07 10:13:29 +0200

    Add: PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+	apps: [
+		{
+			name: 'blog-backend',
+			script: 'dist/index.js',
+
+			// Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+			instances: 'max',
+			auto_restart: true,
+			watch: true,
+			max_memory_restart: '128M',
+			exec_mode: 'cluster',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+			error_file: 'logs/pm2/api_err.log',
+			out_file: 'logs/pm2/api_out.log',
+			log_file: 'logs/pm2/api_combined.log',
+			time: true,
+		},
+	],
+};
