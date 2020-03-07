c739ee2cf4f9100532447576eb5ffcf4abd9e5d6
phr3nzy <adilosama47@gmail.com>
2020-03-07 12:26:37 +0200

    Refactor: schema to be Swagger spec compliant

--- lib/routes/users/index.ts
+++ lib/routes/users/index.ts
@@ -10,27 +10,34 @@ export const userRoutes = [
 		schema: {
 			response: {
 				200: {
+					description: 'Successfully requested all users',
 					type: 'object',
 					properties: {
 						users: {
 							type: 'array',
+							description: 'Users',
 							items: {
 								type: 'object',
 								properties: {
 									id: {
 										type: 'integer',
+										description: 'User ID',
 									},
 									fullName: {
 										type: 'string',
+										description: 'User full name',
 									},
 									email: {
 										type: 'string',
+										description: 'User email',
 									},
 									createdAt: {
 										type: 'string',
+										description: 'Date when the user was created',
 									},
 									updatedAt: {
 										type: 'string',
+										description: 'Date when the user was last updated',
 									},
 								},
 							},
@@ -39,18 +46,22 @@ export const userRoutes = [
 				},
 				500: {
 					type: 'object',
+					description: 'Fail to get all users',
 					properties: {
 						error: {
 							type: 'object',
 							properties: {
 								name: {
 									type: 'string',
+									description: 'Error name',
 								},
 								message: {
 									type: 'string',
+									description: 'Error message',
 								},
 								statusCode: {
 									type: 'integer',
+									description: 'Error status code',
 								},
 							},
 						},
@@ -80,70 +91,89 @@ export const userRoutes = [
 				properties: {
 					id: {
 						type: 'integer',
+						description: 'User ID',
 					},
 				},
 			},
 			response: {
 				200: {
 					type: 'object',
+					description: 'Successfully requested a user',
 					properties: {
 						user: {
 							type: 'object',
 							properties: {
 								id: {
 									type: 'integer',
+									description: 'User ID',
 								},
 								fullName: {
 									type: 'string',
+									description: 'User full name',
 								},
 								email: {
 									type: 'string',
+									description: 'User email',
 								},
 								articles: {
 									type: 'array',
+									description: 'Articles belonging to the user',
 									items: {
 										type: 'object',
 										properties: {
 											id: {
 												type: 'integer',
+												description: 'Article ID',
 											},
 											title: {
 												type: 'string',
+												description: 'Article title',
 											},
 											body: {
 												type: 'string',
+												description: 'Article body',
 											},
 											category: {
 												type: 'object',
+												description: 'Category of the article',
 												properties: {
 													id: {
 														type: 'integer',
+														description: 'Category ID',
 													},
 													name: {
 														type: 'string',
+														description: 'Category name',
 													},
 													createdAt: {
 														type: 'string',
+														description: 'Date when the category was created',
 													},
 													updatedAt: {
 														type: 'string',
+														description:
+															'Date when the category was last updated',
 													},
 												},
 											},
 											createdAt: {
 												type: 'string',
+												description: 'Date when the article was created',
 											},
 											updatedAt: {
 												type: 'string',
+												description: 'Date when the article was last updated',
 											},
 										},
 									},
 								},
 								createdAt: {
 									type: 'string',
+									description: 'Date when the user was created',
 								},
 								updatedAt: {
 									type: 'string',
+									description: 'Date when the user was last updated',
 								},
 							},
 						},
@@ -151,18 +181,22 @@ export const userRoutes = [
 				},
 				404: {
 					type: 'object',
+					description: 'Fail to get a user because they were not found',
 					properties: {
 						error: {
 							type: 'object',
 							properties: {
 								name: {
 									type: 'string',
+									description: 'Error name',
 								},
 								message: {
 									type: 'string',
+									description: 'Error message',
 								},
 								statusCode: {
 									type: 'integer',
+									description: 'Error status code',
 								},
 							},
 						},
@@ -170,18 +204,23 @@ export const userRoutes = [
 				},
 				500: {
 					type: 'object',
+					description: 'Fail to get a user',
 					properties: {
 						error: {
 							type: 'object',
+							description: 'Error',
 							properties: {
 								name: {
 									type: 'string',
+									description: 'Error name',
 								},
 								message: {
 									type: 'string',
+									description: 'Error message',
 								},
 								statusCode: {
 									type: 'integer',
+									description: 'Error status code',
 								},
 							},
 						},
