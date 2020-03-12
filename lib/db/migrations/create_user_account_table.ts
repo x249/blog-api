e3072374b4b36f3746be5214c2fe44bfe929cf1c
phr3nzy <adilosama47@gmail.com>
2020-03-12 23:05:49 +0200

    Add: unique prop on users email field (migration)

--- lib/db/migrations/create_user_account_table.ts
+++ lib/db/migrations/create_user_account_table.ts
@@ -4,7 +4,10 @@ exports.up = ((knex, promise) => {
 	return knex.schema.createTable('user_account', table => {
 		table.bigIncrements('id');
 		table.string('full_name', 256).notNullable();
-		table.string('email', 256).notNullable();
+		table
+			.string('email', 256)
+			.notNullable()
+			.unique();
 		table.string('password').notNullable();
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
