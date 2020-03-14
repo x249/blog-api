98bf440443bc8d30aa7a3a7c443fc2b4af5779c7
phr3nzy <adilosama47@gmail.com>
2020-03-14 10:14:32 +0200

    Add: dev env knexfile config

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,13 @@
 import config from './lib/config';
 
 module.exports = {
+	development: {
+		client: 'pg',
+		connection: config.dbUrl,
+		migrations: {
+			directory: './lib/db/migrations',
+		},
+	},
 	testing: {
 		client: 'pg',
 		connection: config.dbUrl,
