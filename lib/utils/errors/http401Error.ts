9ff321bf273a15d1541c766621e8a9610feebc9f
phr3nzy <adilosama47@gmail.com>
2020-03-14 10:14:08 +0200

    Fix: typo on 401 err name

--- lib/utils/errors/http401Error.ts
+++ lib/utils/errors/http401Error.ts
@@ -1,7 +1,7 @@
 import { HTTPClientError } from './httpClientError';
 
 export class HTTP401Error extends HTTPClientError {
-	public readonly name: string = 'ERR_UNAUTHROIZED';
+	public readonly name: string = 'ERR_UNAUTHORIZED';
 	public readonly statusCode: number = 401;
 
 	constructor(message: string | object = 'Unauthorized') {
