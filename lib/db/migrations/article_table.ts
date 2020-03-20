ce8b7b0b548e56231df3d057741c53f7e5c16a74
phr3nzy <adilosama47@gmail.com>
2020-03-20 17:09:22 +0200

    Add: article & category table migrations

---
+++ lib/db/migrations/article_table.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+exports.up = ((knex, Promise) =>
+	knex.schema.createTable('article', table => {
+		table.bigIncrements('id');
+		table.bigInteger('user_id').notNullable();
+		table.bigInteger('category_id').notNullable();
+		table
+			.string('title', 256)
+			.notNullable()
+			.unique();
+		table.text('body').notNullable();
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(knex.fn.now());
+	})) as (knex: Knex, Promise: PromiseConstructor) => Knex.SchemaBuilder;
+
+exports.down = ((knex, Promise) =>
+	knex.schema.dropTableIfExists('article')) as (
+	knex: Knex,
+	Promise: ProxyConstructor,
+) => Knex.SchemaBuilder;
---
+++ lib/db/migrations/category_table.ts
@@ -0,0 +1,18 @@
+import Knex from 'knex';
+
+exports.up = ((knex, Promise) =>
+	knex.schema.createTable('category', table => {
+		table.bigIncrements('id');
+		table
+			.string('name', 256)
+			.notNullable()
+			.unique();
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(knex.fn.now());
+	})) as (knex: Knex, Promise: PromiseConstructor) => Knex.SchemaBuilder;
+
+exports.down = ((knex, Promise) =>
+	knex.schema.dropTableIfExists('category')) as (
+	knex: Knex,
+	Promise: PromiseConstructor,
+) => Knex.SchemaBuilder;
