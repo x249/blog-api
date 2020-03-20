99955497c819180575b35162b3673c0b3808b8db
phr3nzy <adilosama47@gmail.com>
2020-03-20 17:09:03 +0200

    Refactor: user_account model migration

--- lib/db/migrations/user_table.ts
+++ lib/db/migrations/user_table.ts
@@ -1,6 +1,6 @@
 import Knex from 'knex';
 
-exports.up = ((knex, promise) => {
+exports.up = ((knex, Promise) => {
 	return knex.schema.createTable('user_account', table => {
 		table.bigIncrements('id');
 		table.string('full_name', 256).notNullable();
@@ -12,8 +12,10 @@ exports.up = ((knex, promise) => {
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 	});
-}) as (knex: Knex, promise: Promise<any>) => Knex.SchemaBuilder;
+}) as (knex: Knex, Promise: PromiseConstructor) => Knex.SchemaBuilder;
 
-exports.down = ((knex, promise) => {
-	return knex.schema.dropTableIfExists('user_account');
-}) as (knex: Knex, promise: Promise<any>) => Knex.SchemaBuilder;
+exports.down = ((knex, Promise) =>
+	knex.schema.dropTableIfExists('user_account')) as (
+	knex: Knex,
+	Promise: PromiseConstructor,
+) => Knex.SchemaBuilder;
